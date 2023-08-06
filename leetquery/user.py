5bf8008f3e451128fa774b4ce4893980a7a02f31
Shu-Yu Huang <b123767195@gmail.com>
2023-08-06 05:03:36 +0000

    add user submission query

---
+++ leetquery/user.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2023 Shu-Yu Huang
+#
+# Licensed under the MIT License
+import requests
+import json
+
+__all__  = ["get_submissions"]
+
+def get_submissions(username="syhaung"):
+    url = "https://leetcode.com/graphql?"
+    headers = {'Content-Type': 'application/json'}
+    query = """
+    query recentAcSubmissions($username: String!, $limit: Int!) {
+        recentAcSubmissionList(username: $username, limit: $limit) {
+            id
+            title
+            titleSlug
+            timestamp
+        }
+    }
+    """
+    variables = {'username': username, 'limit': 50}
+    payload = {
+            'query': query,
+            'operationName': 'recentAcSubmissions',
+            'variables': variables
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(payload)).json()
+    return [x["title"] for x in response["data"]["recentAcSubmissionList"]]
